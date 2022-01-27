8ef571b302377da07ab1f2814bd7176150f5787a
Emrah Güvenkaya <guvenkayaemrah52@gmail.com>
2022-01-27 22:05:24 +0300

    trigonometri
    trigonometri

---
+++ dosya_5/hesap.py
@@ -0,0 +1,62 @@
+#Trigonometri hesapalama aracı (Bunu yapmam biraz zor olacak çünkü trigonometre bilmiyorum xd)
+
+from ast import Break
+from math import *
+from urllib.request import install_opener
+
+print("""
+--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽-
+
+                _/﹋\_
+                (҂`_')
+                <,︻╦╤─ ҉* _ _ 
+                _/﹋\_
+
+        trigonometri hesaplama aracı
+        (çıkmak için q'ya basın.)
+
+        İşlemler:
+            1.Kosinüs
+            2.Sinüs
+            3.Tanjant
+            4.Kotanjant
+
+
+                      /|
+                     / |
+                    /  |
+                   /   |
+                  /    |
+                 /     | 
+                /      |
+               /       |
+              /        |
+             /         |
+            /          |
+            ⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺
+        
+--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽-
+""")
+
+while True:
+    
+
+    islem = str(input("İşlemi girin(1-4): "))
+
+    #yarın if durumlarını ayarlayacağım. hata vermemesi için şimdilik sadece işlemi yazdırıyorlar. (27.01.2022)
+    
+    if islem == "1":
+        print(islem)
+    elif islem == "2":
+        print(islem)
+    elif islem == "3":
+        print(islem)
+    elif islem == "4":
+        print(islem)
+    elif islem == "q":
+        print("Programdan çıkılıyor..")
+        break
+    else:
+        print("Lütfen geçerli bir işlem giriniz..")
+
+
