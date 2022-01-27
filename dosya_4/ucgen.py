8dd3ded00b5ad7a122822fe26abc0b79f540ca7a
Emrah Güvenkaya <guvenkayaemrah52@gmail.com>
2022-01-27 14:46:43 +0300

    hesap
    asdk

--- dosya_4/ucgen.py
+++ dosya_4/ucgen.py
@@ -1,22 +1,25 @@
-#Şimdi de geometrik şekil hesaplama işlemi yapalım. İlk olarak kullanıcıdan üçgenin mi dörtgenin mi tipini bulmak istediğini sorun.
-
-#Eğer kullanıcı "Dörtgen" cevabını verirse , 4 tane kenar isteyip bu dörtgenin kare mi , dikdörtgen mi yoksa sıradan bir dörtgen mi olduğunu bulmaya çalışın.
-
-#Eğer kullanıcı "Üçgen" cevabını verirse , 3 tane kenar isteyip bu üçgenin ikizkenar mı , eşkenar mı yoksa sıradan bir üçgen mi olduğunu bulmaya çalışın. Eğer verilen kenarlar bir üçgen belirtmiyorsa, ekrana "Üçgen belirtmiyor" şeklinde bir yazı yazın.o
-
-#Üçgen belirtme şartını bilmiyorsunuz internetten bakabilirsiniz.
-
-#Ayrıca , bu problemde mutlak değer bulmaya ihtiyacınız olacak. Bunun için, Pythonda hazır bir fonksiyon olan abs() fonksiyonunu kullanabilirsiniz. Kullanımı şu şekildedir ;
-
-#EmrahGK
-
 print("""---------------------------------------
            HESAPLAMA ARACI
         (çıkmak için q'ya basın)
 ---------------------------------------""")
 
+durum=" "
+eskenar = "ve bu şekil bir eşkenar üçgendir."
+ikizkenar = "ve bu şekil bir ikizkenar üçgendir"
+diger = "ve bu şekil sıradan bir üçgendir"
+
+def ucgen(a,b,c):
+    global durum
+    if(a == b and a == c):
+        durum = eskenar
+    if(a == b or b == c or a == c):
+        durum = ikizkenar
+    else:
+        durum = diger
+    return durum
+  
 while True:
-        
+  
     soru = str(input("üçgen mi dörtgen mi?: "))
 
 
@@ -39,38 +42,27 @@ while True:
             break
 
     if(soru == "Üçgen" or soru == "üçgen"):
-        
+  
         bir = int(input("Birinci kenarı gir: "))
         iki = int(input("İkinci kenarı gir: "))
         uc =  int(input("Üçüncü kenarı gir: "))
 
-        def ucgen(bir,iki,uc):
-            if(bir == iki and bir == uc):
-                print("bu şekil bir eşkenar üçgendir.")
-            if(bir == iki or iki == uc or bir == uc):
-                print("bu şekil bir ikizkenar üçgendir")
-            else:
-                print("bu şekil sıradan bir üçgendir")
-        
+        ucgen(bir,iki,uc)
+
         if(abs(iki-uc) < bir < iki + uc):
-            print("Kenarlarını girdiğiniz şekil üçgen belirtmektedir ve")
+            print("Kenarlarını girdiğiniz şekil üçgen belirtmektedir",durum)
             break
         if(abs(bir-uc) < iki < bir + uc):
-            print("Kenarlarını girdiğiniz şekil üçgen belirtmektedir ve")
+            print("Kenarlarını girdiğiniz şekil üçgen belirtmektedir",durum)
             break
         if(abs(bir-iki) < uc < bir + iki):
-            print("Kenarlarını girdiğiniz şekil üçgen belirtmektedir ve",ucgen(bir,iki,uc))
+            print("Kenarlarını girdiğiniz şekil üçgen belirtmektedir",durum)
             break
         else:
             print("Kenarlarını girdiğiniz şekil üçgen belirtmemektedir, tekrar deneyin")
-        
+  
     if(soru == "q"):
         break
-    
+ 
     else:
         print("Lütfen geçerli bir işlem girin..")
-
-
-
-
-
