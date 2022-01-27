7a9a978e8b5ec4b7c29299ebe8a34fa24ee47ce4
Emrah Güvenkaya <guvenkayaemrah52@gmail.com>
2022-01-28 00:25:06 +0300

    trigonometri
    trigonometri

--- dosya_5/hesap.py
+++ dosya_5/hesap.py
@@ -1,8 +1,7 @@
 #Trigonometri hesapalama aracı (Bunu yapmam biraz zor olacak çünkü trigonometre bilmiyorum xd)
 
-from ast import Break
-from math import *
-from urllib.request import install_opener
+#bu kodu hazır math modülü sayesinde biraz daha koddan tasarruf edebildiğimizi göstermek için yazdım. Diğer dosyada kısa olan
+#kullanım var.
 
 print("""
 --⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽-
@@ -12,8 +11,8 @@ print("""
                 <,︻╦╤─ ҉* _ _ 
                 _/﹋\_
 
-        trigonometri hesaplama aracı
-        (çıkmak için q'ya basın.)
+        Trigonometri hesaplama aracı
+          (çıkmak için q'ya basın.)
 
         İşlemler:
             1.Kosinüs
@@ -26,36 +25,75 @@ print("""
                      / |
                     /  |
                    /   |
-                  /    |
-                 /     | 
+                  /    |   
+     HİPOTENÜS   /     | KARŞI
                 /      |
                /       |
               /        |
              /         |
-            /          |
+            /\'α'       |
             ⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺⎺
-        
+               KOMŞU
+
 --⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽--⎻⎺⎺⎻--⎽__⎽-⎻⎺⎺⎻-⎽__⎽-
 """)
 
+global a,b,c
+
+def kosinus(a,b,c):
+    return c / a
+
+def sinus(a,b,c):
+    return b / a
+
+def tanjant(a,b,c):
+    return b / c
+
+def kotanjant(a,b,c):
+    return c / b  
+        
 while True:
-    
 
     islem = str(input("İşlemi girin(1-4): "))
 
+    hipotenus = int(input("Hipotenüs'ü girin: "))
+    karsi = int(input("Karşı kenarı girin: "))
+    komsu = int(input("Komşu kenarı girin: "))
+
     #önemli not: yarın if durumlarını ayarlayacağım. hata vermemesi için şimdilik sadece işlemi yazdırıyorlar. (27.01.2022)
 
-    if islem == "1":
-        print(islem)
-    elif islem == "2":
-        print(islem)
-    elif islem == "3":
-        print(islem)
-    elif islem == "4":
-        print(islem)
-    elif islem == "q":
+    if(islem == "1"):
+        #Kosinüs işlemi
+        print("İşlem Kosinüs(yanlış girdiyseniz q'ya basın)")
+        if(hipotenus == "q" or karsi == "q" or komsu == "q"):
+            break
+        print("Kosinüs(α) = ",kosinus(hipotenus,karsi,komsu))
+
+    elif(islem == "2"):
+        #Sinüs işlem
+        print("İşlem Sinüs(yanlış girdiyseniz q'ya basın)")
+        if(hipotenus == "q" or karsi == "q" or komsu == "q"):
+            break
+        print("Sinüs(α) = ",sinus(hipotenus,karsi,komsu))
+
+    elif(islem == "3"):
+        #3.Tanjant işlemi
+        print("İşlem Tanjant(yanlış girdiyseniz q'ya basın)")
+        if(hipotenus == "q" or karsi == "q" or komsu == "q"):
+            break
+        print("Tanjant(α) = ",tanjant(hipotenus,karsi,komsu))
+        
+    elif(islem == "4"):
+        #4.Kotanjant işlemi
+        print("İşlem Kotanjant5(yanlış girdiyseniz q'ya basın)")
+        if(hipotenus == "q" or karsi == "q" or komsu == "q"):
+            break
+        print("Kotanjant(α) = ",kotanjant(hipotenus,karsi,komsu))
+
+    elif(islem == "q"):
         print("Programdan çıkılıyor..")
         break
+
     else:
         print("Lütfen geçerli bir işlem giriniz..")
 
