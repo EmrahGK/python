32c3ad374a8974150f507d893c811957268d27ee
Emrah Güvenkaya <guvenkayaemrah52@gmail.com>
2022-02-03 00:49:27 +0300

    .
    .

---
+++ dosya_9/int_bulucu.py
@@ -0,0 +1,24 @@
+"""
+Elinizde stringlerin bulunduğu bir liste bulunduğunu düşünün.
+
+liste = ["345","sadas","324a","14","kemal"]
+
+Bu listenin içindeki stringlerden içinde sadece rakam bulunanları ekrana yazdırın.
+Bunu yaparken try,except bloklarını kullanmayı unutmayın."""
+
+eleman1 = input("Birinci eleman: ")
+eleman2 = input("İkinci eleman: ")
+eleman3 = input("Üçüncü eleman: ")
+eleman4 = input("Dördüncü eleman: ")
+eleman5 = input("Beşinci eleman")
+
+# kendime not: eleman1 = index olarak 0'dır
+liste = [eleman1,eleman2,eleman3,eleman4,eleman5]
+
+for i in liste:
+
+    try:
+        i = int(i)
+        print(i)
+    except:
+        continue
